a673c47d8b7dd086b647a7e986ce44ba92f9f2cb
Armin <armin.jamali@imtek.uni-freiburg.de>
2025-07-31 14:27:50 +0200

    a new puzzle is there!

--- W1T3_Fuctions.cpp
+++ W1T3_Fuctions.cpp
@@ -5,6 +5,26 @@ using namespace std;
 void funktion1();
 void funktion2(int x);
 void funktion3(int i, double d);
+int max(int a, int b, int c);
+int max2(int a, int b, int c);
+void forDemo();
+void doWhileDemo();
+
+
+/*
+Jede Funktion hat eine Signatur!
+Eine Signatur besteht aus dem Funktionsnamen und den Datentypen
+der Argumente in der richtigen Reihenfolge.
+Der Rückgabedatentyp gehört - im Gegensatz zu vielen Meinungen -
+nicht zur Signatur!
+Die Signatur dieser Funktion ist also max (int, int, int).
+
+Ist eine Funktion mit demselben Namen, aber unterschiedlicher 
+Signatur vorhanden, wird die Funktion als "überladen"
+bezeichnet. Hier ist "max" zweifach überladen.
+*/
+
+
 
 int main()
 {
@@ -20,6 +40,11 @@ int main()
     int i=1;
     double d=3.14;
     funktion3(i,d);
+
+    //doWhileDemo();
+    forDemo();
+
+    cout <<  max(1,2,3) << endl;
     return 0;
 }
 
@@ -55,6 +80,102 @@ void funktion2(int x)
 void funktion3(int i, double d)
 {
     cout << "In funktion2(int): i = " << i << ", d = " << d << endl;
+}
+
+// ------------------------------------------------------------------
+
+int max(int a, int b, int c)
+{
+    cout << "max(int, int, int)\n";
+    int m;
+
+    m = (a > b) ? (a > c) ?
+    a : ((b > c) ? b : c) : c;
+
+    //m = max(a, b);
+    //m = max(m, c);
+
+    return m;
+}
+
+// ------------------------------------------------------------------
+
+int max2(int a, int b, int c)
+{
+    cout << "max(int, int, int)\n";
+    int m;
+
+    m = (a > b) ? (a > c) ? a : c
+        : (b > c) ? b : c;
+
+
+    
+
+    //m = max(a, b);
+    //m = max(m, c);
+
+    return m;
+}
+
+// ------------------------------------------------------------------
+
+// ------------------------------------------------------------------
+
+/*
+do
+{
+    ...
+} while (<prädikat>);
+
+Während bei einer while - Schleife die Bedingung im Schleifenkopf
+steht, ist sie in einer do - while - Schleife im Fuss untergebracht.
+Das hat zur Folge, daß in diese Schleife ohne Bedingung eingetreten
+wird und das Prädikat erst nach dem Schleifenkörper ausgewertet
+wird.Eine do - while - Schleife wird folglich mindestens einmal
+durchlaufen.
+*/
+void doWhileDemo()
+{
+    int i = 1;
+
+    do
+    {
+        cout << "(01) i = " << i << endl;
+        i++;
+    } while (i <= 5);
+    cout << "(01) Nach Schleife: i = " << i << endl << endl;
+
+    i = 0;
+
+    do
+    {
+        i++;
+        cout << "(02) i = " << i << endl;
+    } while (i <= 4);
+    cout << "(05) Nach Schleife: i = " << i << endl << endl;
+}
+
+// ------------------------------------------------------------------
+
+/*
+While-Schleifen sind ideal, wenn die Anzahl der Durchläufe nicht
+bekannt ist.
+Das ist bei for-Schleifen anders. Sie haben einen Startwert, zählen
+um eine bestimmte Schrittweite hoch oder runter und besitzen eine
+Abbruchbedingung.
+
+Sie haben die Grundstruktur
+for (Startwert; Bedingung; Wertänderung)
+{
+    ...
+}
+
+Kurz: for (A; B; C) { K } . Der Ablauf: A B K C B K C B K C ... K C B.
+*/
+void forDemo()
+{
+    for (int i = 1; i <= 5; i++)
+        cout << "(01) i = " << i << endl;
 }
 
 // ------------------------------------------------------------------
---
+++ W1T4_Uebung_Schleifen_Funktionen.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+
+using namespace std;
+
+
+/*
+1)
+Es ist eine Funktion zu schreiben, die einen Primzahltest durchführt.
+Sie erhält einen int-Wert und liefert einen bool-Wert zurück.
+
+2)
+Eine Funktion "nextPrime" erhält einen int-Wert und gibt ihren Primzahl-Nachfolger 
+zurück:
+cout << nextPrime(7) << endl;
+gibt "11" aus
+
+nextPrime(7) -> 11
+nextPrime(11) -> 13
+nextPrime(12) -> 13
+nextPrime(13) -> 17
+
+Für die Schleife können nun alle heute bekanntgewordenen Anweisungen 
+verwendet werden.
+Tipp: sinnvollerweise wird die Funktion aus Teilaufgabe 1) verwendet.
+*/
+
+
+bool is_prime (unsigned int a);
+unsigned int next_prime (unsigned int a);
+
+
+
+int main()
+{
+    setlocale(LC_ALL, "de_DE.utf-8");
+    cout.setf(ios::boolalpha);
+
+    unsigned int a = 23;
+    unsigned int b = next_prime(a);
+
+    cout << "The prime number after " << a << " is: " << b << endl;
+
+/*     for (int j = 10; j > 0; j--)
+    {
+        cout << "The prime numbers after " << a << " are: " << next_prime(a) << endl;
+        a = next_prime(a);
+    } */
+    return 0;
+}
+
+// --------------------------------------------------------------------------------
+// Functions
+// --------------------------------------------------------------------------------
+bool is_prime (unsigned int a)
+{
+    unsigned int d = 2;
+    bool primeCheck = true;
+
+    if (a < 2)
+    {
+        primeCheck = false;
+        return primeCheck;
+    }
+
+    while ( d*d <= a )
+    {
+        if ( (a%d) == 0 )
+        {
+            primeCheck = false;
+            return primeCheck;
+        }
+        d++;
+    }
+    return primeCheck;
+}
+
+// --------------------------------------------------------------------------------
+
+unsigned int next_prime (unsigned int a)
+{
+    unsigned int nextPrime;
+
+    a++;
+    while (!is_prime(a))
+    {
+        a++;
+    }
+
+    nextPrime = a;
+    return nextPrime;
+}
+
+// --------------------------------------------------------------------------------
